a0a46e71c5bf827c22dec9a363744bf1edc048e7
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-04 11:04:41 -0500

    player data displayed to the screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Players from './components/GetPlayers'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Women's World Cup</h1>
+      <Players/>
     </div>
   );
 }
--- client/src/components/GetPlayers.js
+++ client/src/components/GetPlayers.js
@@ -1,2 +1,35 @@
 import React from 'react';
 import axios from 'axios';
+
+class Players extends React.Component{
+    state = {
+        player: []
+    };
+    componentDidMount(){
+        axios.get(`http://localhost:5000/api/players`)
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                player: res.data
+            })
+        })
+        .catch(err => {
+            console.log('no data for u', err)
+        })
+    };
+
+
+    render(){
+        return(
+            <div className='playerCard'>
+               {this.state.player.map(item => {
+                   return(
+                       <h3 key={item.id}>{item.name}</h3>
+                   )
+               })}
+            </div>
+        )
+    }
+}
+
+export default Players;
