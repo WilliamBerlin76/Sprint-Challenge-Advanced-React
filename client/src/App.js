f4bf31dc803731fb908235142368fff2bafe79e1
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-04 11:15:44 -0500

    created redMode custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import Players from './components/GetPlayers'
+import {useRedMode} from './hooks/useRedMode'
 import './App.css';
 
 function App() {
+  const [redMode, redModeChanges, setRedMode] = useRedMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setRedMode(!redMode)
+  }
+
   return (
     <div className="App">
       <h1>Women's World Cup</h1>
+      <button onClick={toggleMode}>Red Mode</button>
       <Players/>
     </div>
   );
